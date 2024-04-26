66028b645463db27929f147ef8ad2e2dbfc380de
Nathan Stéwill LOUFOUA <127401929+LNStewill@users.noreply.github.com>
2024-04-26 17:31:05 +0200

    Ajout Package Interface Graphique

---
+++ Personnel/src/interfaceGraphique/PersonnelInterface.java
@@ -0,0 +1,14 @@
+package interfaceGraphique;
+
+import javax.swing.*;
+
+public class PersonnelInterface {
+	
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello World");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(250, 250);
+        frame.setVisible(true);
+    }
+
+}
